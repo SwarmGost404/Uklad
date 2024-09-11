38dcfd00a281bc154333c0be24b81ade37518321
uhi <vik.z.c.r@yandex.ru>
2024-09-11 15:36:20 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,51 @@
+const card1 = document.getElementById("card1")
+const card2 = document.getElementById("card2")
+const card3 = document.getElementById("card3")
+
+const data1 = document.getElementById("data1")
+const data2 = document.getElementById("data2")
+const data3 = document.getElementById("data3")
+
+const comm1 = document.getElementById("comment1")
+const comm2 = document.getElementById("comment2")
+const comm3 = document.getElementById("comment3")
+
+const map1 = document.getElementById("map1")
+const map2 = document.getElementById("map2")
+const map3 = document.getElementById("map3")
+
+const help1 = document.getElementById("help1")
+const help2 = document.getElementById("help2")
+const help3 = document.getElementById("help3")
+
+let dataMer1 = '17 Октября'
+let dataMer2 = '22 Октября'
+let dataMer3 = '10 Ноября'
+
+let mapMero1 = 'Луга'
+let mapMero2 = 'ПМК Берёзка'
+let mapMero3 = 'Г. Мышкин'
+
+let coment1 = "Фестиваль 'Русский дух' \n приходить строго после 6"
+let coment2 = "Фестиваль 'Русский дух' \n приходить строго после 6"
+let coment3 = "Симейный круг \n приходить строго 18+"
+
+let merMap1 = "https://yandex.ru/maps/-/CDDFn02v"
+let merMap2 = "https://yandex.ru/maps/-/CDDFrRP~"
+let merMap3 = "https://yandex.ru/maps/-/CDDFrGm~"
+
+data1.innerText = dataMer1
+data2.innerText = dataMer2
+data3.innerText = dataMer3
+
+comm1.innerText = coment1
+comm2.innerText = coment2
+comm3.innerText = coment3
+
+map1.innerText = mapMero1
+map2.innerText = mapMero2
+map3.innerText = mapMero3
+
+help1.href = merMap1
+help2.href = merMap2
+help3.href = merMap3
